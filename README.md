8ff6e06016927c3d2dd8714a34aa007f5d9509e7
jesse keane <me@jessekeane.me>
2014-03-03 19:38:49 -0800

    templatize me, capn

--- README.md
+++ README.md
@@ -8,6 +8,11 @@ evaluate js with ziggy
 evaluate `<code>` and output the result. if there is no result, output up to 5
 of the `console.log()`s.. or an error
 
+## additional functionality
+
+any expression within `${...}` will be evaluated and the resulting string said
+to the channel (or private message, depending on how it was invoked)
+
 ## license
 
 MIT
--- index.js
+++ index.js
@@ -1,21 +1,68 @@
 var Sandbox = require('sandbox')
   , sandbox = new Sandbox()
 
+var template_rex = /\$\{\s*(.*?)\s*\}/g
 module.exports = ziggy_eval
 
 function ziggy_eval(ziggy) {
   ziggy.on('message', parse_message)
+  ziggy.on('pm', parse_pm)
 
   function parse_message(user, channel, message) {
     var bits = message.split(' ')
       , command = bits[0]
       , code
 
+    if (template_rex.test(message) && command !== 'js') return templatize()
     if (command !== '!js') return
 
     code = bits.slice(1).join(' ')
 
     sandbox.run(code, do_output)
+
+    function templatize() {
+      var replacements = []
+        , count = 0
+
+      var code_chunk
+
+      message = message.replace(template_rex, replace_code)
+
+      for (var i = 0, l = replacements.length; i < l; ++i) {
+        code_chunk = replacements[i]
+        
+        run_and_insert(code_chunk.code, code_chunk.offset)
+      }
+
+      function replace_code(chunk, code, offset) {
+        ++count
+
+        replacements.push({
+            offset: offset
+          , code: code
+        })
+
+        return ''
+      }
+
+      function run_and_insert(code, offset) {
+        sandbox.run(code, do_insert)
+
+        function do_insert(output) {
+          message = [
+              message.slice(0, offset)
+            , output.result
+            , message.slice(offset)
+          ].join('')
+
+          if(--count === 0) finally_talk()
+        }
+      }
+
+      function finally_talk() {
+        ziggy.say(channel, '"' + message + '"')
+      }
+    }
 
     function do_output(output) {
       if ((!output.result || output.result === 'null') &&
@@ -31,5 +78,9 @@ function ziggy_eval(ziggy) {
 
       ziggy.say(channel, user.nick + ': invalid request!')
     }
+  }
+
+  function parse_pm(user, message) {
+    return parse_message(user, user.nick, message)
   }
 }
