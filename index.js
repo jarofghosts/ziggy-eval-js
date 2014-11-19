ba2d933e5cf83aef90c32cbd14c1499324ad31ee
jesse keane <me@jessekeane.me>
2014-11-18 17:19:49 -0800

    fix output length

--- index.js
+++ index.js
@@ -1,46 +1,47 @@
 var Sandbox = require('sandbox')
-  , sandbox = new Sandbox()
 
-var template_rex = /\$\{\s*(.*?)\s*\}/g
+var sandbox = new Sandbox()
 
-ziggy_eval.help = [
+var templateRex = /\$\{\s*(.*?)\s*\}/g
+
+ziggyEval.help = [
     '!js <code> - evaluate <code> and return result'
   , 'use ${ <code> } anywhere in a message to evaluate <code>'
 ].join('\n')
 
-module.exports = ziggy_eval
+module.exports = ziggyEval
 
-function ziggy_eval(ziggy) {
-  ziggy.on('message', parse_message)
-  ziggy.on('pm', parse_pm)
+function ziggyEval(ziggy) {
+  ziggy.on('message', parseMessage)
+  ziggy.on('pm', parsePm)
 
-  function parse_message(user, channel, message) {
+  function parseMessage(user, channel, message) {
     var bits = message.split(' ')
       , command = bits[0]
       , code
 
-    if (template_rex.test(message) && command !== 'js') return templatize()
-    if (command !== '!js') return
+    if(templateRex.test(message) && command !== 'js') return templatize()
+    if(command !== '!js') return
 
     code = bits.slice(1).join(' ')
 
-    sandbox.run(code, do_output)
+    sandbox.run(code, doOutput)
 
     function templatize() {
       var replacements = []
         , count = 0
 
-      var code_chunk
+      var codeChunk
 
-      message = message.replace(template_rex, replace_code)
+      message = message.replace(templateRex, replaceCode)
 
-      for (var i = 0, l = replacements.length; i < l; ++i) {
-        code_chunk = replacements[i]
-        
-        run_and_insert(code_chunk.code, code_chunk.offset)
+      for(var i = 0, l = replacements.length; i < l; ++i) {
+        codeChunk = replacements[i]
+
+        runAndInsert(codeChunk.code, codeChunk.offset)
       }
 
-      function replace_code(chunk, code, offset) {
+      function replaceCode(chunk, code, offset) {
         ++count
 
         replacements.push({
@@ -51,42 +52,46 @@ function ziggy_eval(ziggy) {
         return ''
       }
 
-      function run_and_insert(code, offset) {
-        sandbox.run(code, do_insert)
+      function runAndInsert(code, offset) {
+        sandbox.run(code, doInsert)
 
-        function do_insert(output) {
+        function doInsert(output) {
           message = [
               message.slice(0, offset)
             , output.result
             , message.slice(offset)
           ].join('')
 
-          if(--count === 0) finally_talk()
+          if(--count === 0) finallyTalk()
         }
       }
 
-      function finally_talk() {
+      function finallyTalk() {
         ziggy.say(channel, '"' + message + '"')
       }
     }
 
-    function do_output(output) {
-      if ((!output.result || output.result === 'null') &&
+    function doOutput(output) {
+      if((!output.result || output.result === 'null') &&
           output.console.length) {
-        for (var i = 0, l = output.console.length; i < l && i < 5; ++i) {
+        for(var i = 0, l = output.console.length; i < l && i < 5; ++i) {
           ziggy.say(channel, output.console[i])
         }
 
         return
       }
 
-      if (output.result) return ziggy.say(channel, output.result)
+      if(output.result) {
+        output.result = output.result.split('\n').slice(0, 5).join('\n')
+
+        return ziggy.say(channel, output.result)
+      }
 
       ziggy.say(channel, user.nick + ': invalid request!')
     }
   }
 
-  function parse_pm(user, message) {
-    return parse_message(user, user.nick, message)
+  function parsePm(user, message) {
+    return parseMessage(user, user.nick, message)
   }
 }
